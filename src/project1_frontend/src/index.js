9b6b30cddca753bbe343db7df76d827a046b2046
Nisha Rai <85097109+Nisharaibxr@users.noreply.github.com>
2023-10-26 23:26:34 +0530

    index.js

--- src/project1_frontend/src/index.js
+++ src/project1_frontend/src/index.js
@@ -1,19 +1,43 @@
-import { project1_backend } from "../../declarations/project1_backend";
+ import { project1_backend} from "../../declarations/project1_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+ window.addEventListener("load", async function(){
+   update();
+ });
 
-  button.setAttribute("disabled", true);
+ document.querySelector("form").addEventListener("submit", async function(event){
+    event.preventDefault();
+    //console.log("Submitted.");
+  
+    const button=  event.target.querySelector("#submit-btn");
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await project1_backend.greet(name);
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
-  button.removeAttribute("disabled");
+    
+ 
+    if(document.getElementById("input-amount").value.length != 0){
+        await project1_backend.topUp(inputAmount);
+    }
 
-  document.getElementById("greeting").innerText = greeting;
+    if(document.getElementById("withdrawal-amount").value.length != 0){
+        await project1_backend.withdraw(outputAmount);
+    }
+ 
+    await project1_backend.compound();
+ 
+    update()
 
-  return false;
+    document.getElementById("input-amount").value ="";
+    document.getElementById("withdrawal-amount").value ="";
+
+    button.removeAttribute("disabled");
+
 });
+
+async function update(){
+    const currentAmount = await project1_backend.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100)/ 100;
+}
+
+ 
